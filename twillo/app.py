eb1342d7cd2c0a4bf0c7a2e87f176ee8e15967a6
hbalickgoodman <56572956+hbalickgoodman@users.noreply.github.com>
2023-04-07 21:53:26 -0400

    updated twillo keys so github ignores it

--- twillo/app.py
+++ twillo/app.py
@@ -1,6 +1,16 @@
 from twilio.rest import Client
+import os
+from dotenv import load_dotenv
 
 # Set up Twilio client with your account SID and auth token
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access environment variables
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+
 client = Client(account_sid, auth_token)
 
 # Define the text you want to convert to speech
@@ -12,7 +22,8 @@ voice = "alice"
 
 # Send the API request to Twilio Voice API
 response = client.calls.create(
-    twiml='<Say voice="{0}" language="{1}">{2}</Say>'.format(voice, language, text),
+    twiml='<Say voice="{0}" language="{1}">{2}</Say>'.format(
+        voice, language, text),
     to='+1234567890',
     from_='+9876543210'
 )
